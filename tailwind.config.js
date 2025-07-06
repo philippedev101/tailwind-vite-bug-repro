ca1dbe545d36db3dc380fb11ae98e9aa4432be7f
philippedev101 <philippedev101@gmail.com>
2025-07-06 20:28:23 +0200

    Add safelist item

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
+  // Add a safelist to test if the config is loaded AT ALL by the vite plugin path.
+  // If this class does NOT appear, it's definitive proof the config is not being found.
+  safelist: [
+    'rotate-12',
+  ],
   content: [
     './index.html',
     './src/**/*.{ts,html}',
